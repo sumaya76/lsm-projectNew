5d2ff2dfd3d3450241074c9d3ed85aebbb5a02d8
sumaya76 <lammi8776@gmail.com>
2025-10-27 23:57:50 +0600

    today i am work on Hero.jsx ,FeaturedCategories.jsx ,FeaturedCourses.jsx,Course.jsx

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Layout from '../common/layout'
+import Hero from '../common/Hero'
+import FeaturedCategories from '../common/FeaturedCategories'
+import FeaturedCourses from '../common/FeaturedCourses'
+import Course from '../common/Course'
+
 export const Home = () => {
   return (
     <div>
    <Layout>
-    <h1>Home</h1>
+  <Hero />
+  <FeaturedCategories />
+    <FeaturedCourses />
+    <Course />
    </Layout>
         </div>
   )
